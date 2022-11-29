a20aee5556253e3848bd24945c452cb8f5ba7a25
Fábio M. Costa <fmc@ufg.br>
2022-11-28 22:38:58 -0300

    Added the use of keys to published events

--- producer.py
+++ producer.py
@@ -4,14 +4,15 @@ import sys
 
 try:
     topic = sys.argv[1]
+    key1 = sys.argv[2]
 except:
-    print ('Usage: python3 producer <topic_name>')
+    print ('Usage: python3 producer <topic_name> <key>')
     exit(1)
     
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 for i in range(100):
     msg = 'My ' + str(i) + 'st message for topic ' + topic
     print ('Sending message: ' + msg)
-    producer.send(topic, value=msg.encode())
+    producer.send(topic, value=msg.encode(), key=key1.encode())
 
 producer.flush()
